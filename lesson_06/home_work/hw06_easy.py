fa3fcf469f5076d506b3165a0d55436a82686b44
vmnchv <vmnchv@gmail.com>
2018-11-05 22:45:44 +0300

    не сделана вторая задача нормал

--- lesson_06/home_work/hw06_easy.py
+++ lesson_06/home_work/hw06_easy.py
@@ -1,5 +1,32 @@
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+import math
+
+class Triangle ():
+    def __init__(self, a_x, a_y, b_x, b_y, c_x, c_y):
+        self.a_x = a_x
+        self.a_y = a_y
+        self.b_x = b_x
+        self.b_y = b_y
+        self.c_x = c_x
+        self.c_y = c_y
+        self.AB = round (math.sqrt(int (math.fabs(((b_y - a_y)**2) + ((b_x - a_x)**2)))),2)
+        self.BC = round(math.sqrt(int(math.fabs(((c_y - b_y) ** 2) + ((c_x - b_x) ** 2)))), 2)
+        self.CA = round(math.sqrt(int(math.fabs(((a_y - c_y) ** 2) + ((a_x - c_x) ** 2)))), 2)
+
+    def perimetr(self):
+        self.perimetr = (self.AB + self.BC + self.CA)
+        return (self.perimetr)
+
+    def ploshad(self):
+        self.perimetr /=2
+        self.ploshad =  round(math.sqrt(self.perimetr*(self.perimetr - self.AB)*(self.perimetr - self.BC)* (self.perimetr - self.CA)),2)
+        return (self.ploshad)
+
+    def vysota(self):
+        self.ploshad *=2
+        self.vysota =  round((self.ploshad / self.CA),2)
+        return (self.vysota)
 
 
 
@@ -8,3 +35,10 @@
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
+my_tri = Triangle(2,2,5,8,7,4)
+
+
+print('Длинна строны АВ = {}, ВС = {}, СА = {}'.format(my_tri.AB, my_tri.BC,my_tri.CA))
+print('Периметр треугольника АВС равен {}'.format(my_tri.perimetr()))
+print('Площадь треугольника АВС равна {}'.format(my_tri.ploshad()))
+print('Высота треугольника АВС, проведенная из угла В равна {}'.format(my_tri.vysota()))
