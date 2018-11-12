8f0d289d94ec2fe351f7a269505a975e9a9a91e5
vmnchv <vmnchv@gmail.com>
2018-11-12 21:46:44 +0300

    openweather

--- lesson_08/home_work/openweather.py
+++ lesson_08/home_work/openweather.py
@@ -122,4 +122,39 @@ OpenWeatherMap — онлайн-сервис, который предостав�
         ...
 
 """
+#  pip install click
+
+import click
+import requests
+
+SAMPLE_API_KEY = 'b1b15e88fa797225412429c1c50c122a1'
+
+
+def current_weather(location, api_key=SAMPLE_API_KEY):
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+
+    query_params = {
+        'q': location,
+        'appid': api_key,
+    }
+
+    response = requests.get(url, params=query_params)
+
+    return response.json()['weather'][0]['description']
+
+
+@click.command()
+@click.argument('location')
+@click.option(
+    '--api-key', '-a',
+    help='your API key for the OpenWeatherMap API',
+)
+def main(location, api_key):
+    weather = current_weather(location, api_key)
+    print(f"The weather in {location} right now: {weather}.")
+
+
+if __name__ == "__main__":
+    main()
+
 
